82e1f1a80fa2ac296237e58f721e7e93ff258b5c
Sahil Singh <sahilsingh20698@gmail.com>
2025-09-17 15:12:57 +0530

    changes in port and url

--- src/api.js
+++ src/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const API = axios.create({
-    baseURL: "http://10.242.213.8:4000"
+    // baseURL: "http://10.242.213.8:4000"
+    baseURL: "http://localhost:4000"
+
 });
 
 API.interceptors.request.use((req) => {
